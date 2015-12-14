d5871cbfe9d96e7d9212acf72a4bb89f7380b320
Amanda Olsen <aolsen@aolsen-mbp.duncllc.com>
2015-12-14 16:39:29 -0600

    main.js - filter method

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // append styles to head
-var oneLineCSS = '<style id="pathOptimizer-styles" type="text/css">#overlay-initial {display: none; position: fixed; top: 0px; left: 0px; width: 100%; height: 100%; z-index: 10000; } #overlay-primary {display: none; font-family: arial, sans-serif; position: fixed; top: 0px; left: 0px; width: 100%; height: 100%; z-index: 100; cursor: pointer; background-image: -moz-linear-gradient(rgba(255, 255, 255, .85), rgba(255, 255, 255, .85)); background-image: -o-linear-gradient(rgba(255, 255, 255, .85), rgba(255, 255, 255, .85)); background-image: -webkit-gradient(linear, left top, left bottom, from(rgba(255, 255, 255, .85)), to(rgba(255, 255, 255, .85))); background-image: linear-gradient(rgba(255, 255, 255, .85), rgba(255, 255, 255, .85)); } #overlay-primary.active {display: block; } .optimizer-container {position: absolute; width: 600px; left: 50%; margin-left: -300px; top: 150px; overflow: hidden; opacity: 0; transition: opacity .5s; border: 2px solid #999; background-color: #eee; border-radius: 6px; padding: 25px; cursor: auto; } .optimizer-container.stat-show {opacity: 1; } .optimizer-container .content {font-size: 14px; } #optimizer-close {position: absolute; right: 6px; top: 5px; color: #999; cursor: pointer; font-size: 20px; padding: 4px; z-index: 1; font-style: normal; } .optimizer-container textarea {font-weight: bold; background-color: transparent; border: 0px; width: 100%; height: 100%; resize: none; outline: none; font-size: 14px; padding: 0px; padding-top: 10px; line-height: 1; overflow: hidden; box-shadow: none; } .optimizer-container p.secondary {margin-top:0px; margin-bottom: 10px; outline: 0; } .optimizer-container p.secondary span {font-weight: bold; color: red; font-size: 14px; } .optimizer-container p.secondary span.once {color: green; } .optimizer-container p.msg {color: #666; font-size: 12px; }</style>';
+var oneLineCSS = '<style id="pathOptimizer-styles" type="text/css">#overlay-initial {display: none; position: fixed; top: 0px; left: 0px; width: 100%; height: 100%; z-index: 10000; } #overlay-primary {display: none; font-family: arial, sans-serif; position: fixed; top: 0px; left: 0px; width: 100%; height: 100%; z-index: 100; cursor: pointer; background-image: -moz-linear-gradient(rgba(255, 255, 255, .85), rgba(255, 255, 255, .85)); background-image: -o-linear-gradient(rgba(255, 255, 255, .85), rgba(255, 255, 255, .85)); background-image: -webkit-gradient(linear, left top, left bottom, from(rgba(255, 255, 255, .85)), to(rgba(255, 255, 255, .85))); background-image: linear-gradient(rgba(255, 255, 255, .85), rgba(255, 255, 255, .85)); } #overlay-primary.active {display: block; } .optimizer-container {position: absolute; width: 600px; left: 50%; margin-left: -300px; top: 150px; overflow: hidden; opacity: 0; transition: opacity .5s; border: 2px solid #999; background-color: #eee; border-radius: 6px; padding: 25px; cursor: auto; } .optimizer-container.stat-show {opacity: 1;} .optimizer-container .content {font-size: 14px; } #optimizer-close {position: absolute; right: 6px; top: 5px; color: #999; cursor: pointer; font-size: 20px; padding: 4px; z-index: 1; font-style: normal; } .optimizer-container textarea {font-weight: bold; background-color: transparent; border: 0px; width: 100%; height: 100%; resize: none; outline: none; font-size: 14px; padding: 0px; padding-top: 10px; line-height: 1; overflow: hidden; box-shadow: none; } .optimizer-container p.secondary {margin-top:0px; margin-bottom: 10px; outline: 0; } .optimizer-container p.secondary span {font-weight: bold; color: red; font-size: 14px; } .optimizer-container p.secondary span.once {color: green; } .optimizer-container p.msg {color: #666; font-size: 12px; } .extraSelectors {margin-top:10px; padding-top:8px;} .extraSelectors div {background-color: #AE849F; padding: 5px 10px 5px 10px; border-radius:10px; margin-bottom:10px;} .extraSelectors .editable {background-color:#E8C7EA; cursor:pointer; transition:background-color 0.5s ease;} .extraSelectors .middleElement {background-color:#E8C7EA;} .extraSelectors .editable.middleElement.added {background-color: #AE849F;} .extraSelectors .editable.added, .extraSelectors .editable:hover {background-color:#AE849F;} .extraSelectors .editable.added.middleElement:hover {background-color:#E8C7EA;} </style>';
 $("head").append(oneLineCSS);
 
 // overlay-initial: not visible but which will, when display:block, block all user events not associated with this script
@@ -26,6 +26,9 @@ $(document).keyup(function(e) {
     }
 });
 
+var path, selector, middleElement, lastElement;
+var internalElements = [];
+
 var main = function(e) {
   	$(document).off("click", main); // allows user to immediately close the overlay // otherwise the main function would fire first 
     $("#overlay-initial").hide(); // remove invisible overlay so that the "element" variable actualy gets the right element 
@@ -36,7 +39,7 @@ var main = function(e) {
     var element = document.elementFromPoint(x, y);
 
   	var id = $(element).closest('[id]');
-  	var path = "";
+  	path = "";
 
   	var multipleClasses = function(classes) {
   		if (classes.indexOf(" ") > -1) {
@@ -61,7 +64,7 @@ var main = function(e) {
 
 	// get last element
 	var lastElementTag = $(element).prop("tagName").toLowerCase();
-	var lastElement = lastElementTag;
+	lastElement = lastElementTag;
 	// if class(es), add those
 	if ($(element).attr("class") != undefined) {
 		var lastElementClass = $(element).attr("class");
@@ -69,19 +72,38 @@ var main = function(e) {
 		lastElement = lastElementTag +'.'+ lastElementClass;
 	} 
 
-  	// construct selector
+	var extraSelectors = '';
+
+  	// get elements before last element
   	if (id.length > 0) {
   		var idName = id.attr("id");
-  		var selector = '$("#'+idName+'")';
+  		selector = '$("#'+idName+'")';
   		if ($(element).is(id)) { // if ID is from current element
-  			var path = selector;
+  			path = selector;
   		} else { // if ID is from a parent
 			if ($(element).parent().is(id)) { // 2 elements
 				path = selector + '.find("'+lastElement+'")';
-			} else { // 3 elements
+			} else { // 3+ elements
 				middleElement = getMiddleElement(element);
 				path = selector + '.find("'+middleElement+ ' ' +lastElement+'")';
-			}
+				if (!$(element).parent().parent().is(id)) { // 4+ elemenets // must contruct internal elements
+					// get count of elements in between 
+					extraSelectors += '<div>'+lastElement+'</div>';
+					extraSelectors += '<div class="middleElement editable data-index=0 added">'+middleElement+'</div>';
+					var parents = eval(path).parentsUntil("#" + idName);
+					parents = parents.slice(1) // select all parts of element except the first one
+					var parentsNames = parents.each(function(index) {
+						// if class(es), add those
+						if ($(this).attr("class") === undefined) {
+							var element = $(this.nodeName).toLowerCase();
+						} else {
+							var element = "." + (this.className).trim().split(' ').join('.');
+						}
+						extraSelectors += '<div class="editable" data-index='+(index+1)+'>'+element+'</div>';
+					})
+					extraSelectors += '<div>#' + idName + '</div>';
+				}
+			} 
   		}
   	} else {
   		// get middle element + class
@@ -107,6 +129,8 @@ var main = function(e) {
 
   	// build/display message
   	var message = '<textarea class="js-copytextarea">'+path+'</textarea><p class="secondary" tabindex="0">'+countText+'</p>';
+  	message += '<button>Edit Selector Path</button>';
+  	message += '<div class="extraSelectors">'+extraSelectors+'</div>';
   	$("#overlay-primary")
   		.show()
   		.find(".content").html(message);
@@ -117,19 +141,64 @@ var main = function(e) {
   	copyTextarea.select();
     var successful = document.execCommand('copy');
     var msg = successful ? 'Path copied to your clipboard.' : 'Unfortunately, path was not copied to your clipboard. Please try again.';
-	    $("#overlay-primary").find(".optimizer-container .content").append('<p class="msg">'+msg+'</p>');
-  	copyTextarea.selectionStart = copyTextarea.selectionEnd = -1; // deselect path
+    $("#overlay-primary").find(".optimizer-container .secondary").after('<p class="msg">'+msg+'</p>');
+  	copyTextarea.selectionStart = copyTextarea.selectionEnd = -1; // deselect path // un-highlights text in input field
   	$("#overlay-primary").find("p.secondary").focus(); // pulls cursor out of textarea
 }
 
 // user action // triggers main function
-$(document).on("keydown", function(e) {
-	if (e.altKey) { // shift key pressed
-		$("#overlay-initial").show(); // overlay page so that click does not trigger any event other than the one below
-        $(document).on("click", main); // run main function
-        setTimeout(function() { // later...
-        	$("#overlay-initial").hide();
-	        $(document).off("click", main); // unattach this function so user can click without triggering the overly
-        }, 1000) // gives the user 1 second after alt to click
-    }
-})
+$(document)
+	.on("keydown", function(e) {
+		if (e.altKey) { // shift key pressed
+			$("#overlay-initial").show(); // overlay page so that click does not trigger any event other than the one below
+	        $(document).on("click", main); // run main function
+	        setTimeout(function() { // later...
+	        	$("#overlay-initial").hide();
+		        $(document).off("click", main); // unattach this function so user can click without triggering the overlay
+	        }, 1000) // gives the user 1 second after alt to click
+	    }
+	})
+	.on("click", ".editable", function() {
+		// if this element has not been added, add it to the array
+		var thisIndex = $(this).attr("data-index");
+		var internalElementsOutput = "";
+
+		var sortAndStringify = function(x) {
+			// sort from largest index to smallest index
+	        x.sort(function(a,b) {
+	            return parseFloat(b.index) - parseFloat(a.index);
+	        });
+	        // stringify
+			$(x).each(function() {
+				internalElementsOutput += this.content + " ";
+			})
+			console.log(internalElementsOutput);
+		}
+
+		if (!$(this).hasClass('added')) {
+			console.log("add a selector");
+			$(this).addClass('added');
+			var group = {};
+			group.index = thisIndex;
+			group.content = $(this).text();
+			internalElements.push(group);
+			sortAndStringify(internalElements);
+		} else {
+			console.log("remove a selector");
+			$(this).removeClass('added');
+			var internalElements1 = internalElements.filter(function(element) {
+				return element.index != thisIndex;
+			})
+			sortAndStringify(internalElements1);
+		}
+
+		// include middleElement as needed
+		if (!$(this).hasClass('middleElement')) {
+			var finalElement = middleElement + ' ' +lastElement;
+		} else {
+			var finalElement = lastElement;
+		}
+		// console.log(internalElementsOutput);
+		var newPath = selector + '.find("' +internalElementsOutput + finalElement+ '")';
+		// console.log(newPath);
+	})
--- readme.md
+++ readme.md
@@ -21,3 +21,6 @@ For more reading: [https://learn.jquery.com/performance/optimize-selectors/](htt
 
 ## Icon
 Icon made by [Freepik](http://www.flaticon.com) and licensed under [CC BY 3.0](http://creativecommons.org/licenses/by/3.0/).
+
+## Change log
+v.1.2 - Added "View intermediate selectors" button. This allows the user to tweak the selector they've been given to something more suitable (since the tool can't know all your reasoning for picking a different selector path).
